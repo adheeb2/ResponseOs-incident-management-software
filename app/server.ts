27e68207d93b4dc72a1f370cde1c93b748215f49
adheeb2 <adheebanvar2@gmail.com>
2026-01-27 13:43:12 +0530

    feat(app):add basic http service

---
+++ app/server.ts
@@ -0,0 +1,29 @@
+import Fastify from 'fastify';
+const app = Fastify({logger:true});//this logger is from pino, it was already installed by fastify.Pino is a logging library which gives out details in json format
+//The level means the severity of log, ranging from 10-60,30 being the normal
+
+app.get('/', async () => {
+    return { hello: 'world' }
+})
+
+
+const start = async () => {
+    try {
+        await app.listen({ port: 3000 }, (error, address) => {
+            if (error) throw error
+            console.log(`server running on ${address}`)
+        });
+    } catch (error) {
+        app.log.error(error)
+        process.exit(1)
+    }
+};
+start();
+
+const closeApp = async () => {
+    await app.close()
+    process.exit(1)
+};
+
+process.on('SIGINT', closeApp);   
+process.on('SIGTERM', closeApp);
