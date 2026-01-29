ab26985726094fad4292ad363af938e43a787ff4
adheeb2 <adheebanvar2@gmail.com>
2026-01-29 18:24:27 +0530

    feat(server):learn request and reply of routes

--- app/server.ts
+++ app/server.ts
@@ -1,18 +1,31 @@
-import Fastify from 'fastify';
-const app = Fastify({logger:true});//this logger is from pino, it was already installed by fastify.Pino is a logging library which gives out details in json format
+import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+const app = Fastify({ logger: true });//this logger is from pino, it was already installed by fastify.Pino is a logging library which gives out details in json format
 //The level means the severity of log, ranging from 10-60,30 being the normal
 
-app.get('/', async () => {
-    return { hello: 'world' }
+app.get('/', {
+    handler: async (request, reply) => {
+        return { message: "hello" }
+    }
 })
+async function userRoutes(fastify: FastifyInstance) {
+    fastify.post("/", {
+        handler: async (request: FastifyRequest<{
+            Body: {
+                name: string, age: string
+            }
+        }>, reply: FastifyReply) => {
+            const body = request.body
+            console.log(body)
+            return reply.code(201).send(body)
+        }
+    })
+}
 
+app.register(userRoutes, { prefix: "/api/users" });
 
 const start = async () => {
     try {
-        app.listen({ port: 3000 }, (error, address) => {
-            if (error) throw error
-            console.log(`server running on ${address}`)
-        });
+        app.listen({ port: 3000 })
     } catch (error) {
         app.log.error(error)
         process.exit(1)
@@ -25,5 +38,5 @@ const closeApp = async () => {
     process.exit(1)
 };
 
-process.on('SIGINT', closeApp);   
+process.on('SIGINT', closeApp);
 process.on('SIGTERM', closeApp);
