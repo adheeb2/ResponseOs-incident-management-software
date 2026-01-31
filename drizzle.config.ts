0196a2467d6c298ae739ae760a7f5927a59e631b
adheeb2 <adheebanvar2@gmail.com>
2026-01-31 14:04:07 +0530

    feat(drizzle):refactor the position of drizzle schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    out: './drizzle',
-    schema: './database/drizzle/src/db/index.ts',
+    out: './database/drizzle/src/db/migrations',
+    schema: './database/drizzle/src/db/schema/index.ts',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
