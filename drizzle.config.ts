8cbd0187b1f64f716dedad4893fbc0fd8b368a18
adheeb2 <adheebanvar2@gmail.com>
2026-01-31 14:06:46 +0530

    feat(drizzle):comment made in drizzle config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    out: './database/drizzle/src/db/migrations',
-    schema: './database/drizzle/src/db/schema/index.ts',
+    out: './database/drizzle/src/db/migrations',//This is where migration gen is stored
+    schema: './database/drizzle/src/db/schema/index.ts',//this is where we get schema for migrations
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
